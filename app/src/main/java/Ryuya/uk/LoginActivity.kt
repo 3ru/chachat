a0be9ae319bccf3af0d5282cd0e4bf4e31f2e77c
Ryuyxx <evrlastingryu@yahoo.co.jp>
2021-01-13 20:06:52 +0900

    run
    3

---
+++ app/src/main/java/Ryuya/uk/LoginActivity.kt
@@ -0,0 +1,32 @@
+package Ryuya.uk
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.acitivity_login.*
+import kotlinx.android.synthetic.main.activity_register.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.acitivity_login)
+
+        login_button_login.setOnClickListener{
+            val email = email_edittext_login.text.toString()
+            val password = password_edittext_login.text.toString()
+
+            Log.d("Login", "Attempt login with email/pw: $email/***")
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+//                .addOnCompleteListener()
+        }
+
+        back_to_register_textview.setOnClickListener{
+            finish()
+        }
+    }
+}
